7a7a59aec2928805126ec7fdcac10fa9ec780a6a
J <equus497@gmail.com>
2024-10-13 13:05:37 -0600

    init commit

---
+++ first.cpp
@@ -0,0 +1,20 @@
+/*
+ *  A simple program that prints out the name
+ *  and address of a fictional character.
+ */
+
+#include <iostream>
+
+// Main function
+int main(int argc, char *argv[]) {
+
+    // Print functions on newlines
+    std::cout << "First Name:\tMax" << std::endl;
+    std::cout << "Last Name:\tRockatansky" << std::endl;
+    std::cout << "Address:\t2015 Fury Road" << std::endl;
+    std::cout << "City:\t\tDarwin, Northern Territory [Australia]" << std::endl;
+    std::cout << "Zipcode:\t90909" << std::endl;
+
+    // Successful exit
+    return 0;
+}
